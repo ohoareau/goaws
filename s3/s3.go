0d550264a1b1993280dbcd61a3a2efa1a5db394a
Olivier Hoareau <oha@greenberets.io>
2025-02-23 14:49:38 +0100

    feat: aws sdk v3

--- s3/s3.go
+++ s3/s3.go
@@ -2,51 +2,63 @@ package s3
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"io"
+	"time"
 )
 
 func Singleton() Service {
 	return Service{
-		PutObject:  PutObject,
-		GetObject:  GetObject,
-		ToJsonFile: ToJsonFile,
+		PutObject:          PutObject,
+		GetObject:          GetObject,
+		ToJsonFile:         ToJsonFile,
+		GetGetPresignedUrl: GetGetPresignedUrl,
 	}
 }
-func CreateConnection() *s3.S3 {
-	sess := session.Must(session.NewSession())
-	return s3.New(sess)
+func CreateConnection() (*s3.Client, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+	return s3.NewFromConfig(cfg), nil
 }
 
 func PutObject(bucket string, key string, final []byte) error {
-	svc := CreateConnection()
+	svc, err := CreateConnection()
+	if err != nil {
+		return err
+	}
 
 	input := &s3.PutObjectInput{
 		Body:                 bytes.NewReader(final),
 		Bucket:               aws.String(bucket),
 		Key:                  aws.String(key),
-		ServerSideEncryption: aws.String("AES256"),
-		StorageClass:         aws.String("STANDARD_IA"),
+		ServerSideEncryption: "AES256",
+		StorageClass:         "STANDARD_IA",
 	}
 
-	_, err := svc.PutObject(input)
+	_, err = svc.PutObject(context.TODO(), input)
 
 	return err
 }
 
 func GetObject(bucket string, key string) ([]byte, error) {
-	svc := CreateConnection()
+	svc, err := CreateConnection()
+	if err != nil {
+		return []byte{}, err
+	}
 
 	input := &s3.GetObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
 	}
 
-	output, err := svc.GetObject(input)
+	output, err := svc.GetObject(context.TODO(), input)
 
 	if err != nil {
 		return nil, err
@@ -74,4 +86,26 @@ func ToJsonFile(bucket string, key string, data interface{}) error {
 	}
 
 	return nil
+}
+
+func GetGetPresignedUrl(bucket string, key string, expiration time.Duration) (string, error) {
+	svc, err := CreateConnection()
+	if err != nil {
+		return "", err
+	}
+
+	presignClient := s3.NewPresignClient(svc)
+	req, err := presignClient.PresignGetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	}, func(opts *s3.PresignOptions) {
+		opts.Expires = expiration
+	})
+	if err != nil {
+		return "", err
+	}
+	if req == nil {
+		return "", errors.New("unable to prepare \"get\" presigned url")
+	}
+	return req.URL, nil
 }
--- s3/types.go
+++ s3/types.go
@@ -1,7 +1,10 @@
 package s3
 
+import "time"
+
 type Service struct {
-	PutObject  func(bucket string, key string, final []byte) error
-	GetObject  func(bucket string, key string) ([]byte, error)
-	ToJsonFile func(bucket string, key string, data interface{}) error
+	PutObject          func(bucket string, key string, final []byte) error
+	GetObject          func(bucket string, key string) ([]byte, error)
+	ToJsonFile         func(bucket string, key string, data interface{}) error
+	GetGetPresignedUrl func(bucket string, key string, expiration time.Duration) (string, error)
 }
